a77d2e2a2029e34dd852c862fc21b2c79142265c
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-26 00:01:17 +0300

    fix:parse logs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,17 +2,18 @@
 """
 a script that reads stdin line by line and computes metrics
 """
+from sys import stdin
 
 
-if __name__ == "__main__":
-    from sys import stdin
-    import re
+def print_statistics():
+    """prints statistics from the beginning"""
+    print(f'Total file size: {total_size}')
+    for code, number in status_codes.items():
+        if number != 0:
+            print(f'{code}: {number}')
 
-    regex_str = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b - \[.*\] " \
-        r"\"\w+\s+/projects/\d+\s+\w+\/1\.1\" \d+ \d+"
 
-    pattern = re.compile(regex_str)
-    status_codes = {
+status_codes = {
         200: 0,
         301: 0,
         400: 0,
@@ -23,33 +24,29 @@ if __name__ == "__main__":
         500: 0
     }
 
-    counter = 0
-    total_size = 0
-    try:
-        for line in stdin:
-            match = pattern.match(line)
-            if match:
-                counter += 1
+counter = 0
+total_size = 0
 
-                file_size = line.split(" ")[-1]
-                status_code = line.split(" ")[-2]
-                status_codes[int(status_code)] += 1
+try:
+    for line in stdin:
+        line = line.strip()
+        counter += 1
 
-                total_size = total_size + int(file_size)
-            else:
-                continue
+        try:
+            log_list = line.split()
+            status_code = int(log_list[-2])
+            file_size = int(log_list[-1])
+        except (ValueError):
+            continue
 
-            if counter == 10:
-                counter = 0
-                print(f"File size: {total_size}")
+        total_size += file_size
 
-                for code, number in status_codes.items():
-                    if number != 0:
-                        print(f"{code}: {number}")
+        if status_code in status_codes:
+            status_codes[status_code] += 1
 
-    except KeyboardInterrupt:
-        print(f"File size: {total_size}")
-        for code, number in status_codes.items():
-            if number != 0:
-                print(f"{code}: {number}")
-        raise
+        if counter % 10 == 0:
+            print_statistics()
+
+except KeyboardInterrupt:
+    print_statistics()
+    raise
