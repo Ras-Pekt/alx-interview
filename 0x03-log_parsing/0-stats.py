1630688db2e7f6567356d52a7e3bb796143a6139
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-26 00:13:10 +0300

    fix:parse logs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,54 @@
 #!/usr/bin/python3
 """
-a script that reads stdin line by line and computes metrics
+Task: 0. Log Parsing
+File: 0x06-log_parsing/0-stats.py
 """
 from sys import stdin
 
 
-def print_statistics():
+def print_statistics(total_size, status_codes):
     """prints statistics from the beginning"""
-    print(f'Total file size: {total_size}')
-    for code, number in status_codes.items():
-        if number != 0:
-            print(f'{code}: {number}')
+    print("File size: " + str(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {str(status_codes[code])}")
 
-
-status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
 
 counter = 0
 total_size = 0
+status_code = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
 try:
     for line in stdin:
-        line = line.strip()
         counter += 1
+        line = line.split()
 
-        try:
-            log_list = line.split()
-            status_code = int(log_list[-2])
-            file_size = int(log_list[-1])
-        except (ValueError):
-            continue
+        if len(line) > 1:
+            total_size += int(line[-1])
 
-        total_size += file_size
+        if len(line) > 2 and line[-2].isnumeric():
+            status_code = line[-2]
+        else:
+            status_code = 0
 
-        if status_code in status_codes:
+        if status_code in status_codes.keys():
             status_codes[status_code] += 1
 
         if counter % 10 == 0:
-            print_statistics()
+            print_statistics(total_size, status_codes)
 
-except KeyboardInterrupt:
-    print_statistics()
+    print_statistics(total_size, status_codes)
+
+except (KeyboardInterrupt):
+    print_statistics(total_size, status_codes)
     raise
