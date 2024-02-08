b2a38e0a2c095f6b63edca93b5fd8a5c5ab7220e
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-08 15:32:39 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,21 +6,20 @@ represents a valid UTF-8 encoding
 
 
 def validUTF8(data):
-    num_bytes = 0
+    byte_count = 0
 
-    for num in data:
-        if num_bytes == 0:
-            if (num >> 5) == 0b110:
-                num_bytes = 1
-            elif (num >> 4) == 0b1110:
-                num_bytes = 2
-            elif (num >> 3) == 0b11110:
-                num_bytes = 3
-            elif (num >> 7):
+    for byte in data:
+        mask = 1 << 7
+        if not byte_count:
+            while byte & mask:
+                byte_count += 1
+                mask >>= 1
+            if not byte_count:
+                continue
+            if byte_count == 1 or byte_count > 4:
                 return False
         else:
-            if (num >> 6) != 0b10:
+            if byte >> 6 != 0b10:
                 return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+        byte_count -= 1
+    return byte_count == 0
