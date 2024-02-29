84054b77b68d64afb7a86876df87c7cd8c8ada59
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-03-01 00:44:04 +0300

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+making change module
+"""
+
+
+def makeChange(coins, total):
+    """"making change"""
+    if total <= 0:
+        return 0
+    count = 0
+    idx = 0
+    coins.sort(reverse=True)
+
+    while total > 0 and idx < len(coins):
+        largest = coins[idx]
+        count += total // largest
+        total %= largest
+        idx += 1
+    if total != 0:
+        return -1
+    return count
