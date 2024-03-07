ad208613731e191e09d23ffddc45fa672d7afe60
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-03-07 23:38:49 +0300

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
-"""solving an interview quiz"""
+"""
+A function to  calculate the perimeter of an island
+"""
 
 
 def island_perimeter(grid):
-    """ returns the perimeter of the island described in grid"""
-
-    perimeter = 0
-
+    """
+    returns the perimeter of the island described in grid
+    """
     rows = len(grid)
     columns = len(grid[0])
     perimeter = 0
@@ -15,12 +15,10 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(columns):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
-                    perimeter += 1
-                if grid[i + 1][j] == 0:
-                    perimeter += 1
-                if grid[i][j - 1] == 0:
-                    perimeter += 1
-                if grid[i][j + 1] == 0:
-                    perimeter += 1
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
     return perimeter
