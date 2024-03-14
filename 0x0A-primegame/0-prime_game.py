4f2692fa97416ccea9456a09632d4379b86e3625
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-03-14 17:39:46 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,27 +12,25 @@ def is_prime(num):
     return True
 
 
-def prime_game(n):
-    """calculates wins in prime game"""
-    primes = [i for i in range(2, n + 1) if is_prime(i)]
-    if len(primes) % 2 == 0:
-        return "Ben"
-    else:
-        return "Maria"
-
-
 def isWinner(x, nums):
-    """returns winner of prime game"""
+    """returns winner"""
+
     if not nums or x < 1 or len(nums) != x:
         return None
 
-    winners = [prime_game(n) for n in nums]
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
+    ben = 0
+    maria = 0
 
-    if maria_wins > ben_wins:
+    for n in nums:
+        primes = [i for i in range(2, n + 1) if is_prime(i)]
+        if len(primes) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
+    if maria > ben:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif ben > maria:
         return "Ben"
     else:
         return None
